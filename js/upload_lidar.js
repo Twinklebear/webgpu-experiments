3e5c2a1ff9d484a0dbe7480e41639fbde5b6fa18
Will Usher <willusher.life@gmail.com>
2020-05-11 14:04:26 -0600

    report load times

--- js/upload_lidar.js
+++ js/upload_lidar.js
@@ -4,6 +4,7 @@ var newLasFileReady = false;
 
 function uploadLAS(file) {
     var reader = new FileReader();
+    var start = performance.now();
     reader.onerror = function() {
         alert("error reading las/laz");
     }
@@ -13,7 +14,8 @@ function uploadLAS(file) {
         console.log(`Bounds = ${newLasFile.bounds}`);
 
         newLasFile.loadPoints();
-        console.log(`Loaded ${newLasFile.numLoadedPoints} (noise discarded)`);
+        var end = performance.now();
+        console.log(`Loaded ${newLasFile.numLoadedPoints} (noise discarded) in ${end - start}ms`);
         newLasFileReady = true;
     }
     reader.readAsArrayBuffer(file[0]);
